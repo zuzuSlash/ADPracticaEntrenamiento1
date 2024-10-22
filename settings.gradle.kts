f23f78d20ecaac4a297c16ee6df62c4103664259
juljimgar <julianj1202@gmail.com>
2024-10-22 17:18:25 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "ADPracticaEntrenamiento1"
+
---
+++ src/main/java/ies/julian/Usuario.java
@@ -0,0 +1,68 @@
+package ies.julian;
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String mail;
+    private boolean activo;
+    private String usuario;
+    private String contrasena;
+
+    Usuario(String nombre, String apellido, String mail, String usuario, String contrasena) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+
+}
