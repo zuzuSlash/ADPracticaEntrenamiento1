70dff11b64e2e4b0d6280ab888ef3f0eb77bc62f
juljimgar <julianj1202@gmail.com>
2024-10-25 20:26:58 +0200

    Commit cuarto

--- src/main/java/ies/julian/App.java
+++ src/main/java/ies/julian/App.java
@@ -9,37 +9,17 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.Objects;
 import java.util.Properties;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
-    public App() throws FileNotFoundException {
-    }
 
+    /*
     private static void conectarEmbebido() throws SQLException {
-        String connectionstring =
-                "jdbc:derby:/home/juljimgar/JJGdb;create=false;user=julian;password=Majnga81$";
+        String connectionstring = "jdbc:derby:/home/juljimgar/JJGdb;create=false;user=julian;password=Majnga81$";
         Connection conn = null;
         conn = DriverManager.getConnection(connectionstring);
-
-        Statement s = conn.createStatement ();
-        PreparedStatement psDelete = null;
-        PreparedStatement psInsertar = null;
-        PreparedStatement psUpdate = null;
-
-        psDelete = conn.prepareStatement( "delete from APP.TABLE_USUARIO");
-
-        /*
-        psInsertar = conn.prepareStatement( "insert into APP.TABLE_USUARIO(COLUMN_NOMBRE, COLUMN_APELLIDO, COLUMN_MAIL, COLUMN_ACTIVO) values (?,?,?,?)");
-        psInsertar.setString(1, "Cristian");
-        psInsertar.setString(2, "Rodriguez Ruiz");
-        psInsertar.setString(3, "RodRuiCris@gmail.com");
-        psInsertar.setBoolean(4, true);
-
-         */
-        psDelete.executeUpdate();
-        s.close();
-
 
         if (conn != null) {
             PreparedStatement pstm = conn.prepareStatement("SELECT * FROM APP.TABLE_USUARIO");
@@ -51,6 +31,67 @@ public class App {
             throw new SQLException("No se ha podido conectar");
         }
     }
+
+     */
+
+    public static void menuCRUD(Connection conn) throws SQLException {
+        Statement s = conn.createStatement();
+        PreparedStatement psDelete = null;
+        PreparedStatement psInsertar = null;
+        PreparedStatement psUpdate = null;
+
+        Scanner scanner = new Scanner(System.in);
+        String opcion = "";
+
+        while (!opcion.equals("5")) {
+            System.out.println("Seleccione una opción:\n1. Escribir\n2. Leer\n3. Modificar\n4. Eliminar\n5. Salir");
+            opcion = scanner.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    System.out.println("Escribir");
+                    psInsertar = conn.prepareStatement("INSERT INTO APP.TABLE_USUARIO(COLUMN_NOMBRE, COLUMN_APELLIDO, COLUMN_MAIL, COLUMN_ACTIVO, COLUMN_USUARIO, COULUMN_CONTRASEÑA) VALUES (?,?,?,?,?,?)");
+                    System.out.print("Escribe un nombre: ");
+                    psInsertar.setString(1, scanner.nextLine());
+                    System.out.print("Escribe apellido: ");
+                    psInsertar.setString(2, scanner.nextLine());
+                    System.out.print("Escribe mail: ");
+                    psInsertar.setString(3, scanner.nextLine());
+                    System.out.print("Escribe activo (true/false): ");
+                    psInsertar.setBoolean(4, scanner.nextBoolean());
+                    scanner.nextLine();
+                    System.out.print("Escribe usuario: ");
+                    psInsertar.setString(5, scanner.nextLine());
+                    System.out.print("Escribe contraseña: ");
+                    psInsertar.setString(6, scanner.nextLine());
+                    psInsertar.executeUpdate();
+                    System.out.println("Registro insertado con éxito.");
+                    break;
+                case "2":
+                    System.out.println("Leer");
+
+                    break;
+                case "3":
+                    System.out.println("Modificar");
+
+                    break;
+                case "4":
+                    System.out.println("Eliminar");
+
+                    break;
+                case "5":
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción no válida, por favor intenta de nuevo.");
+                    break;
+            }
+        }
+        s.close();
+        scanner.close();
+    }
+
+
 
 
 
@@ -68,14 +109,17 @@ public class App {
         String password = encryptor.decrypt(props.getProperty("database.password"));
         Connection conn = null;
         conn = DriverManager.getConnection(connectionstring+";"+"user="+user+";"+"password="+password+";");
+
         if(conn!=null){
             System.out.println("Conectado");
         }
+
+        menuCRUD(conn);
         conn.close();
 
 
 
-        conectarEmbebido();
+        //conectarEmbebido();
     }
 
 }
